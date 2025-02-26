47bd0ae27582790f394227b0fb22e37e479d83c8
wilmer <wrpachonl@gmail.com>
2025-02-26 15:52:02 -0800

    Finished portfolio

--- src/components/section/connect.tsx
+++ src/components/section/connect.tsx
@@ -6,7 +6,7 @@ const ConnectSection = () => {
         <h4 className="text-sm uppercase tracking-wide text-gray-400">What's Next?</h4>
         <h2 className="text-4xl font-bold text-purple-400 mt-2">Let’s Connect!</h2>
         <p className="mt-4 max-w-2xl text-lg text-gray-300">
-          I’m currently open to new opportunities and always excited to collaborate. 
+          I&apos;m currently open to new opportunities and always excited to collaborate. 
           Whether you have a job opportunity, a project idea, or just want to chat, 
           feel free to reach out—I’d love to hear from you!
         </p>
