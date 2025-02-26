58aee1ab1f2b5581b92e797ac98dfb5434ac9b1e
wilmer <wrpachonl@gmail.com>
2025-02-26 15:57:01 -0800

    Finished portfolio

--- src/components/section/connect.tsx
+++ src/components/section/connect.tsx
@@ -3,12 +3,12 @@ import Button from "@/components/ui/Button";
 const ConnectSection = () => {
     return (
       <section id="contact" className="flex flex-col items-center text-center text-white py-16 px-6 w-full">
-        <h4 className="text-sm uppercase tracking-wide text-gray-400">What's Next?</h4>
+        <h4 className="text-sm uppercase tracking-wide text-gray-400">What&apos;s Next?</h4>
         <h2 className="text-4xl font-bold text-purple-400 mt-2">Let’s Connect!</h2>
         <p className="mt-4 max-w-2xl text-lg text-gray-300">
           I&apos;m currently open to new opportunities and always excited to collaborate. 
           Whether you have a job opportunity, a project idea, or just want to chat, 
-          feel free to reach out—I’d love to hear from you!
+          feel free to reach out—I&apos;d love to hear from you!
         </p>
 
         <Button 
